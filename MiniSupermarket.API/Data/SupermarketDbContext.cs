e22f2dc04f47ca8731b1492e18ef5ab20f5d6ea8
Admin <thainc26081969@gmail.com>
2026-09-26 17:02:52 +0700

    cai dat 3 goi NU Package Microsoft.EntityFrameworkCore

---
+++ MiniSupermarket.API/Data/SupermarketDbContext.cs
@@ -0,0 +1,38 @@
+﻿/*
+ *  ho ten sinh vien: Nguyen Van Teo
+ *  ma sinh vien: 123456789
+ *  Ngay tao: 15/09/2026 
+ *  mo ta: thuc the San Pham, tuong ung voiw table Products tong CSDL
+ */
+using Microsoft.EntityFrameworkCore;
+using MiniSupermarket.API.Models;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace MiniSupermarket.API.Data
+{
+    // DbContext đại diện cho phiên làm việc với cơ sở dữ liệu SQL Server
+    public class SupermarketDbContext : DbContext
+    {
+        public SupermarketDbContext(DbContextOptions<SupermarketDbContext> options) : base(options) { }
+
+        // Khai báo các bảng dữ liệu ánh xạ từ Model
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        // Cấu hình dữ liệu mồi ban đầu (Data Seeding)
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Nạp sẵn 5 danh mục ban đầu vào SQL Server ngay khi tạo bảng
+            modelBuilder.Entity<Category>().HasData(
+                new Category { CategoryId = 1, CategoryName = "Bánh kẹo & Đồ ăn vặt", Description = "Snack, bánh quy, kẹo dẻo" },
+                new Category { CategoryId = 2, CategoryName = "Nước giải khát & Trà", Description = "Nước ngọt, nước khoáng, trà" },
+                new Category { CategoryId = 3, CategoryName = "Sữa & Sản phẩm từ sữa", Description = "Sữa tươi, sữa chua, phô mai" },
+                new Category { CategoryId = 4, CategoryName = "Mì gói & Thực phẩm ăn liền", Description = "Mì ăn liền, phở khô, cháo gói" },
+                new Category { CategoryId = 5, CategoryName = "Gia vị & Dầu ăn", Description = "Nước mắm, hạt nêm, dầu thực vật" }
+            );
+        }
+    }
+}
